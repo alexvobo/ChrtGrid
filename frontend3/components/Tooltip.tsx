afe92af7a3feadbecf36e61f42fa5b536ab042e0
alexvobo <vobos2@gmail.com>
2022-03-05 00:32:36 -0500

    ui changes

---
+++ frontend3/components/Tooltip.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { InformationCircleIcon } from "@heroicons/react/outline";
+export default function Tooltip() {
+  return (
+    <>
+      <button
+        data-tooltip-target="tooltip-animation"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        Animated tooltip
+      </button>
+      <div
+        id="tooltip-animation"
+        role="tooltip"
+        className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+        Tooltip content
+        <div className="tooltip-arrow" data-popper-arrow></div>
+      </div>
+    </>
+  );
+}
